0f75e62a3d9dee30ede0addde51598fa2778f843
Mohamed Amine Merzouk <mamerzouk@protonmail.com>
2021-04-21 16:26:13 -0400

    add ip addr to run_all.sh

--- simulation_vm/simulation/remote_io/modbus/run_all.sh
+++ simulation_vm/simulation/remote_io/modbus/run_all.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-
-sim_path=/home/user/GRFICSv2/simulation_vm/simulation/remote_io/modbus
+sudo ip addr add 192.168.95.10/24 dev eth0
+sudo ip addr add 192.168.95.11/24 dev eth0
+sudo ip addr add 192.168.95.12/24 dev eth0
+sudo ip addr add 192.168.95.13/24 dev eth0
+sudo ip addr add 192.168.95.14/24 dev eth0
+sudo ip addr add 192.168.95.15/24 dev eth0
+sim_path=/opt/simulation/simulation/remote_io/modbus
 sudo pkill python
 sudo python $sim_path/feed1.py &
 sudo python $sim_path/feed2.py &
